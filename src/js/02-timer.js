51de3ea63754bc7f28367bbda6c3555fc1eb8074
DimaSvirin <115728392+DimaSvirin@users.noreply.github.com>
2023-04-05 11:37:26 +0300

    update task 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -6,27 +6,19 @@ import Notiflix from 'notiflix';
 
 const inputEl = document.querySelector('#datetime-picker');
 const startBtn = document.querySelector('[data-start]');
-const elementDay = document.querySelector('[data-days]');
-const elementHour = document.querySelector('[data-hours]');
-const elementMinute = document.querySelector('[data-minutes]');
-const elementSecond = document.querySelector('[data-seconds]');
+const elDay = document.querySelector('[data-days]');
+const elHour = document.querySelector('[data-hours]');
+const elMinute = document.querySelector('[data-minutes]');
+const elSecond = document.querySelector('[data-seconds]');
 const divEl = document.querySelector('.timer');
-const divElAll = divEl.querySelectorAll('.field');
-console.log(divElAll);
+
+
 
 divEl.style.cssText =
-  'font-size: 60px; font-weight: bolder; display: flex; justify-content: space-around; margin-top: 100px;';
-
-divElAll.forEach(
-  e =>
-    (e.style.cssText =
-      'display: flex; flex-direction: column; align-items: center;')
-);
+  'font-size: 40px; font-weight: bolder;';
 
 const startDate = Date.now();
 let futereDate = 0;
-let colorStart = false;
-let intervalForColor = null;
 
 startBtn.setAttribute('disabled', 'true');
 
@@ -40,14 +32,11 @@ const options = {
     futereDate = selectedDates[0].getTime();
     if (futereDate < startDate) {
       startBtn.setAttribute('disabled', 'true');
+
       Notiflix.Notify.init({
         position: 'center-top',
-        warning: {
-          background: 'yellow',
-          textColor: 'red',
-        },
       });
-      Notiflix.Notify.warning('Please choose a date in the future');
+      Notiflix.Notify.failure('Please choose a date in the future');
     }
   },
 };
@@ -58,22 +47,21 @@ startBtn.addEventListener('click', onStartBtnClick);
 
 function onStartBtnClick() {
   startBtn.setAttribute('disabled', 'true');
-  changeColor();
+  
   const timerId = setInterval(() => {
     const currentTime = Date.now();
     const deltaTime = futereDate - currentTime;
-    // console.log(deltaTime);
-
+    
     if (deltaTime < 900) {
-      clearInterval(intervalForColor);
+      
       startBtn.setAttribute('disabled', 'true');
       clearInterval(timerId);
     }
     const textTime = convertMs(deltaTime);
-    elementSecond.textContent = textTime.seconds;
-    elementMinute.textContent = textTime.minutes;
-    elementHour.textContent = textTime.hours;
-    elementDay.textContent = textTime.days;
+    elSecond.textContent = textTime.seconds;
+    elMinute.textContent = textTime.minutes;
+    elHour.textContent = textTime.hours;
+    elDay.textContent = textTime.days;
   }, 1000);
 }
 
@@ -100,18 +88,4 @@ function convertMs(ms) {
 
 function addLeadingZero(value) {
   return String(value).padStart(2, '0');
-}
-
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
-
-function changeColor() {
-  if (colorStart) {
-    return;
-  }
-  colorStart = true;
-  intervalForColor = setInterval(() => {
-    divEl.style.color = getRandomHexColor();
-  }, 1000);
 }
